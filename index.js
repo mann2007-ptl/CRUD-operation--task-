162c419810cd5b1848c0f04f38162ba56bd28d99
mann2007-ptl <mann.patel.cg@gmail.com>
2026-02-17 16:13:11 +0530

    added cors middleware

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const users = [
   {att:80 , uid : 108243 , total_sub : 14 , bonus : 20 , name : "Dax"},
